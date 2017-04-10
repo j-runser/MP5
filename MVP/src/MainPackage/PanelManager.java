b10bfa4567d6cd212f5e185517f181dff99587cd
Joe Runser <j-runser@onu.edu>
2017-04-10 19:15:56 -0400

    Made main thing

---
+++ MVP/src/MainPackage/PanelManager.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MainPackage;
+
+import java.util.ArrayList;
+import javax.swing.JLayeredPane;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author josephrunser
+ */
+public class PanelManager {
+    
+    ArrayList<JPanel> panels;
+    
+    public static final int INDEX_MAIN_PANEL = 0;
+    public static final int INDEX_LESSON_ONE = 1;
+    public static final int INDEX_LESSON_TWO = 2;
+    
+    StartPanel startPanel;
+    LessonOnePanel lessonOnePanel;
+    LessonTwoPanel lessonTwoPanel;
+    
+    JLayeredPane pane;
+    
+    public PanelManager(JLayeredPane pane) {
+        
+        this.pane = pane;
+        
+        panels = new ArrayList();
+        
+        startPanel = new StartPanel(this);
+        lessonOnePanel = new LessonOnePanel(this);
+        lessonTwoPanel = new LessonTwoPanel(this);
+        
+        panels.add(INDEX_MAIN_PANEL, startPanel);
+        panels.add(INDEX_LESSON_ONE, lessonOnePanel);
+        panels.add(INDEX_LESSON_TWO, lessonTwoPanel);
+        
+    }
+
+    public void setPanel(int index) {
+        
+        panels.get(index).setLocation(0, 0);
+        panels.get(index).setSize(pane.getSize());
+        
+        pane.removeAll();
+        pane.add(panels.get(index));
+        pane.setVisible(true);
+        
+    }
+    
+}
